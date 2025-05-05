92d7795591b016ffa594f8f88eb35fa4c98d0d25
lie_feld <a.musledinova@gmail.com>
2025-05-05 11:50:06 +0700

    KS-5-8. Task 8 - mr comment fixes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,14 @@
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import kotlin.text.Typography.quote
 
 const val URL = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
 
 fun main() {
-
     val doc: Document = Jsoup.connect(URL).get()
 
-    val quote = doc.select(".sc-2aegk7-2.bzpNIu")
+    val quote = doc.select("article")
 
     for (quotes: Element in quote) {
         println("* ${quotes.text()}")
